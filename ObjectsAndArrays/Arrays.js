30b1df1331144f0728b248e6658f8ad93eb4327f
juancalle <pablogaca2@Juans-MacBook-Pro.local>
2020-04-15 16:25:29 -0500

    Arrays are objects

---
+++ ObjectsAndArrays/Arrays.js
@@ -0,0 +1,17 @@
+//Arrays are a data structure
+
+//They are commonly used for ordered data like lists
+
+let person = []
+person.name = 'Don Calle'
+
+let who = person.name
+
+console.log(who)
+
+//Fundamentally, arrays are an object
+if(typeof person === 'array'){
+  console.log('Yesssss! is an array')
+} else{
+  console.log('Nopi!')
+}

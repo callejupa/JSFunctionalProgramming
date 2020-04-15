52918716941958f8fe2682b87059f65be962bd5c
juancalle <pablogaca2@Juans-MacBook-Pro.local>
2020-04-15 15:57:43 -0500

    Dot like a property of access

---
+++ ObjectsAndArrays/propertyAccess.js
@@ -0,0 +1,13 @@
+//Asignaments with Dots
+//Properties can be assigned and accessed using dot notation
+//everything that ever uses a dot in Javascript is an object
+var person = {}
+person.name = "Don Calle"
+person.lastname = 'García'
+
+//the primitive values get pass by value
+//the non-primiteve values pass by reference
+
+console.log(person)
+
+

4494a76c8000d26dcbcf265581e3358faf83963e
juancalle <pablogaca2@Juans-MacBook-Pro.local>
2020-04-15 21:11:03 -0500

    Destructuring with ES6

---
+++ ObjectsAndArrays/destructuringES6.js
@@ -0,0 +1,11 @@
+//Destructuring 
+//A simplified method to extract multiple properties from an object
+//variables declaration
+const [first, second] = [true, false]
+// onleft [the target] = on right [the source]
+
+//Objects declaration
+let {first1, second1} = {first_1: 0, second_1: 1}
+
+//with spread operator
+let [first2, ...second2] = [10, 20, 30, 40]
